ba6bc36692528a582e7afb22fc5b0d9753f946e1
dessHub <deskip95@gmail.com>
2017-01-18 09:47:19 +0300

    altered the drive function

--- Car_lab/car.py
+++ Car_lab/car.py
@@ -1,34 +1,49 @@
+
 class Car(object):
+    
+    def __init__(self, name=None, model=None, vehicle_type=None):
+
+        self.speed = 0
+
+        if name is not None and (name == 'Porshe' or name == 'Koenigsegg'):
+            self.num_of_doors = 2
+        elif name is not None and (name != 'Porshe' or name != 'Koenigsegg'):
+            self.num_of_doors = 4
+        else:
+            pass
+
+        if name is None:
+            self.name = 'General'
+            self.num_of_doors = 4
+        else:
+            self.name = name
+
+        if model is None:
+            self.model = 'GM'
+        else:
+            self.model = model
+
+        if vehicle_type is None:
+            self.vehicle_type = 'saloon'
+        else:
+            self.vehicle_type = vehicle_type
+
+        if vehicle_type == 'trailer':
+            self.num_of_wheels = 8
+        else:
+            self.num_of_wheels = 4
 
 
- def __init__(self, name='General', model='GM', vehicle_type=None,speed = 0):
-   self.name = name
-   self.speed = speed
-   self.model = model
-   self.vehicle_type = vehicle_type
 
+    def is_saloon(self):
 
-   if (name == 'Porshe') or (name == 'Koenigsegg'):
-     self.num_of_doors = 2
-   else:
-     self.num_of_doors = 4
+        if self.vehicle_type == 'saloon':
+            return True
+        else:
+            return False
 
-   if vehicle_type == 'trailer':
-     self.num_of_wheels = 8
-   else:
-     self.num_of_wheels = 4
-
-
- def is_saloon(self):
-   if self.vehicle_type is not 'trailer':
-       self.vehicle_type == 'saloon'
-       return True
-   return False
-
- def drive(self, moving_speed):
-   if moving_speed == 3:
-     self.speed = 1000
-   elif moving_speed == 7:
-     self.speed = 77
-
-   return self
+    def drive(self,moving_speed ):
+        if self.vehicle_type == 'saloon':
+           self.speed = 10 ** moving_speed
+        else:
+           self.speed = 77
